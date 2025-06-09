0b7f10034fb511eda7df1d4da113d5d7c61dc9ee
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-06-09 15:50:43 +0530

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import documentationController from '../controllers/documentation.controller';
+import documentationController from '../controllers/documentation.controller.js';
 import handler from '../utils/handler';
 
 // controllers
